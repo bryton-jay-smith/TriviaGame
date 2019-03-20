05471eca220b0fa3187d2a985004672d9b6fe1b8
bryton smith <bryton.smith15@gmail.com>
2019-03-20 10:07:22 -0700

    Formatting and basic JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,22 @@
-/* Javascript File */
+$(document).ready(function() {
 
-console.log("External JS Loaded - main.js")
+// Create and insert Answer boxes
+  function insertAnswers () {
+    var letters = ['a', 'b', 'c', 'd'];
+    for (i = 0; i < letters.length; i++) {
+      var aInput = $('<input>');
+      aInput.attr('type', 'radio');
+      aInput.attr('class', 'answer');
+      aInput.attr('value', letters[i]);
+      aInput.attr('id', 'answer_' + letters[i]);
+      $('#answers').append(aInput);
+    };
 
-$(document).ready(function() { console.log("JQuery Loaded");
-
-
+    var submit = $('<button>');
+    submit.attr('id', 'submit');
+    submit.text("Submit");
+    $('#answers').append(submit);
+  };
+// End Create and insert Answer boxes
 
 });
